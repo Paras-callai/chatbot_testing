3328a8a3bdd7334a4d3cc5764e5bd6795a8591ae
paras malik <paras.malik@call-ai.com>
2022-10-28 17:46:54 +0530

    initial commit

---
+++ testing_utils.py
@@ -0,0 +1,20 @@
+import requests
+import json
+import os
+import streamlit as st
+
+def bot_response(url, data ):
+
+    payload = json.dumps(data)
+    headers = {
+    'Content-Type': 'application/json'
+    }
+
+    response = requests.request("POST", url, headers=headers, data=payload)
+
+    return response.json()
+
+def file_selector(folder_path):
+    filenames = os.listdir(folder_path)
+    selected_filename = st.selectbox('Select a file', filenames)
+    return os.path.join(folder_path, selected_filename)
