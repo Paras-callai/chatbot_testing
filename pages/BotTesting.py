f391e492ffd27c934b305636846a8a46fd0634e6
paras malik <paras.malik@call-ai.com>
2022-10-31 12:38:22 +0530

    updated

--- pages/BotTesting.py
+++ pages/BotTesting.py
@@ -4,19 +4,19 @@ from testing_utils import *
 from rapidfuzz import process
 import requests
 import pandas as pd
+import time
+
+st.set_page_config(
+page_title="Testing",
+layout="wide"
+)
 
 st.write("""# Chat Bot Testing""")
-# col1, col2 = st.columns(2)
-# with col1:
-#     bot_server = st.text_input('Enter Server IP', '35.228.104.43')
-#     meta_ph_no = st.text_input('Phone Number you need to pass', '9999999999')
-# with col2:
-#     bot_port = st.text_input('Enter Server PORT', '8891')
 
 saved_bot = pd.read_csv("./saved_bot.csv")
 bot_list = saved_bot['bot_name'].tolist()
 
-options = st.selectbox('Select Bot',bot_list)
+options = st.selectbox('Select Bot need to Test',bot_list)
 # bot_webhook = saved_bot[saved_bot['bot_name'] ==options]["bot_webhook"]
 bot_server = saved_bot[saved_bot['bot_name'] ==options]["server_ip"].values[0]
 bot_port = saved_bot[saved_bot['bot_name'] ==options]["server_port"].values[0]
@@ -24,99 +24,117 @@ bot_port = saved_bot[saved_bot['bot_name'] ==options]["server_port"].values[0]
 bot_webhook = f"http://{bot_server}:{bot_port}/webhooks/rest/webhook"
 st.info(bot_webhook, icon="ℹ️")
 
-uploaded_file = st.file_uploader("Choose a file", type =["txt"])
+# Create selectbox
+dir_list = os.listdir("./testflows/")
+options = [i for i in dir_list] + ["Upload File"]
+selection = st.selectbox("Select or Upload a flow you need to test", options=options)
 
-if uploaded_file is not None:
-    count =0
+# Create text input for user entry
+if selection == "Upload File": 
+    other_option = st.file_uploader("Choose a file", type =["txt"])
 
+
+# Just to show the selected option
+if selection != "Upload File":
+    # uploaded_file = selection
+    stringio = read_file(f"/home/paras/Chatbot_testing/testflows/{selection}")
+else: 
+    uploaded_file = other_option
     # To read file as bytes:
-    bytes_data = uploaded_file.read()
-    # To convert to a string based IO:
-    stringio = uploaded_file.getvalue().decode("utf-8")
+    if uploaded_file is not None:
+        bytes_data = uploaded_file.read()
+        # To convert to a string based IO:
+        stringio = uploaded_file.getvalue().decode("utf-8")
+    else:
+        stringio = None
 
+
+if stringio is not None:
+    count =0
     y = stringio.split("Test Start")[1:]
     options = st.multiselect(
         'Select flows',[x for x in y if x != "" or x != " "],[x for x in y if x != "" or x != " "])
-    st.write('You selected:', options)
+    selection_expander = st.expander('You selected:')
+    selection_expander.write(options)
 
     if st.button('Run Test'):
         count =0
-        for d in options:
-            bot_fail ={}
-            if d != "":
-                count_pass=0
-                count_fail=0
-                refined_data = []
-                for i in d.split("\n"):
-                    s = i.split(":")
-                    for x in s[0]:
-                        if x =="I" and s[1] == " /restart":
-                            url = bot_webhook
-                            data ={
-                                "sender":f"test{count}",
-                                "message":s[1],
-                            }
-                            response = requests.post(url ,json= data).json()
-                            continue
-                        if x =="I":
-                            url = bot_webhook
-                            data ={
-                                "sender":f"test{count}",
-                                "message":s[1],
-                               }
-                            response = requests.post(url ,json= data).json()
-                            print(response)
-                            print(len(response.get("messages")))
-                            
-                            if len(response.get("messages")) >0:
-                                rresponse =[]
-                                for i in range(len(response.get("messages"))):
-                                    print(i)
-                                    rresponse.append(response.get("messages")[i].get("payload").get("display"))
+        with st.spinner('Wait for it...'):
+            for d in options:
+                bot_fail ={}
+                if d != "":
+                    count_pass=0
+                    count_fail=0
+                    refined_data = []
+                    for i in d.split("\n"):
+                        s = i.split(":")
+                        for x in s[0]:
+                            if x =="I" and s[1] == " /restart":
+                                url = bot_webhook
+                                data ={
+                                    "sender":f"test{count}",
+                                    "message":s[1],
+                                }
+                                response = requests.post(url ,json= data).json()
+                                continue
+                            if x =="I":
+                                url = bot_webhook
+                                data ={
+                                    "sender":f"test{count}",
+                                    "message":s[1],
+                                }
+                                response = requests.post(url ,json= data).json()
+                                print(response)
+                                print(len(response.get("messages")))
                                 
-                                rresponse = "".join(rresponse)
-                                refined_data.append(rresponse)
-                            
+                                if len(response.get("messages")) >0:
+                                    rresponse =[]
+                                    for i in range(len(response.get("messages"))):
+                                        print(i)
+                                        rresponse.append(response.get("messages")[i].get("payload").get("display"))
+                                    
+                                    rresponse = "".join(rresponse)
+                                    refined_data.append(rresponse)
                                 
+                                    
 
-                        elif x == "O":
-                            if len(refined_data)>0:
-                                relation_map = [process.extractOne(s[1], refined_data)[0] if process.extractOne(s[1], refined_data)[1]>95 else None]
-                                if relation_map[0] != None:
-                                    count_pass +=1
-                                    print ("test passed")
-                                    print ("from bot : " ,refined_data[0])
-                                    print ("from test: ", s[1])
-                                    refined_data.remove(refined_data[0])
-                                else:
-                                    count_fail += 1
-                                    print ("failed at: ")
-                                    print ("from Bot: ", refined_data[0])
-                                    print ("from test: ", s[1])
-                                    bot_fail.update({str(count_fail)+" From Test Case:":s[1],str(count_fail)+" From Bot:":refined_data[0]})
-                                    print("BOT FAIL DICT",bot_fail)
-                            else: 
-                                print(" Refined Data is empty ", refined_data)
-            print ("test passed",count_pass)
-            print ("test failed" ,count_fail)
-            print(f"__________________FLow {count} passed___________")
-            
-            count+=1
-            if count_fail ==0:
-                st.success(f"Flow {count} passed",  icon="✅")
-                st.write("test passed ",count_pass)
-                st.write("test failed ",count_fail)
-            else:
-                # st.write("Flow ",count," failed at ",[i for i in refined_data])
-                error = "Flow "+str(count)+" failed"
-                error = st.error(error,icon="🚨")
-                expander =st.expander("Failed Cases:")
-                expander.write(bot_fail)
-                # st.write(f"Flow {count} Falied at",refined_data)
-                st.write("test passed ",count_pass)
-                st.write("test failed ",count_fail)
-
-            
+                            elif x == "O":
+                                if len(refined_data)>0:
+                                    relation_map = [process.extractOne(s[1], refined_data)[0] if process.extractOne(s[1], refined_data)[1]>95 else None]
+                                    if relation_map[0] != None:
+                                        count_pass +=1
+                                        print ("test passed")
+                                        print ("from bot : " ,refined_data[0])
+                                        print ("from test: ", s[1])
+                                        refined_data.remove(refined_data[0])
+                                    else:
+                                        count_fail += 1
+                                        print ("failed at: ")
+                                        print ("from Bot: ", refined_data[0])
+                                        print ("from test: ", s[1])
+                                        bot_fail.update({str(count_fail)+" From Test Case:":s[1],str(count_fail)+" From Bot:":refined_data[0]})
+                                        print("BOT FAIL DICT",bot_fail)
+                                else: 
+                                    print(" Refined Data is empty ", refined_data)
+                print ("test passed",count_pass)
+                print ("test failed" ,count_fail)
+                print(f"__________________FLow {count} passed___________")
+                
+                count+=1
+                if count_fail ==0:
+                    st.success(f"Flow {count} passed",  icon="✅")
+                    st.write("test passed ",count_pass)
+                    st.write("test failed ",count_fail)
+                else:
+                    # st.write("Flow ",count," failed at ",[i for i in refined_data])
+                    error = "Flow "+str(count)+" failed"
+                    error = st.error(error,icon="🚨")
+                    expander =st.expander("Failed Cases:")
+                    expander.write(bot_fail)
+                    # st.write(f"Flow {count} Falied at",refined_data)
+                    st.write("test passed ",count_pass)
+                    st.write("test failed ",count_fail)
+        st.success('Done!')    
 
 
 
--- testing_utils.py
+++ testing_utils.py
@@ -3,18 +3,21 @@ import json
 import os
 import streamlit as st
 
+
 def bot_response(url, data ):
 
     payload = json.dumps(data)
     headers = {
     'Content-Type': 'application/json'
     }
-
     response = requests.request("POST", url, headers=headers, data=payload)
-
     return response.json()
 
 def file_selector(folder_path):
     filenames = os.listdir(folder_path)
     selected_filename = st.selectbox('Select a file', filenames)
     return os.path.join(folder_path, selected_filename)
+
+def read_file(file_path):
+    with open(file_path, 'r') as f:
+        return f.read()
